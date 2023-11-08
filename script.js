7395cfb7afc6762d3e17617af6970df39e46ba95
Charles Totoro <crtotoro@gmail.com>
2023-11-08 16:15:29 -0600

    Add pseudo code for gameStatus function

--- script.js
+++ script.js
@@ -8,12 +8,27 @@ for(let box of boxes) box.addEventListener('click', e => {
   imgO.src = "./assets/O.png";
   imgX.src = "./assets/X.png";
 
-  if(lastMove === 'o') {
-    box.append(imgX);
-    lastMove = 'x';
-  } else {
-    box.append(imgO);
-    lastMove = 'o';
-  }
+  if(isAvailable(box)) {
+    if(lastMove === 'o') {
+      box.append(imgX);
+      lastMove = 'x';
+    } else {
+      box.append(imgO);
+      lastMove = 'o';
+    }
+  } 
 
 })
+
+function isAvailable(box) {
+  return box.children.length ? true : false
+}
+
+function gameStatus() {
+  // for each space
+  // check horizontal, vertical and diagonal neighbors
+  // if 3 consecutive matching squares, game is won
+  // else if all squares occupied, game is draw
+  // else game is still active
+  // return winner, draw, or active
+}
