2fc9f3aaa4430b97396fc6fc1bbfd06a18f8917d
Charles Totoro <crtotoro@gmail.com>
2023-11-08 16:03:37 -0600

    Fix moving Os in event listener

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const imgO = document.createElement('img');
-imgO.src = "./assets/O.png";
-const imgX = document.createElement('img');
-imgX.src = "./assets/X.png";
 
 const boxes = document.getElementsByClassName("box");
 for(let box of boxes) box.addEventListener('click', e => {
-  let box = e.target;
+  const imgO = document.createElement('img');
+  const imgX = document.createElement('img');
+  const box = e.target;
+  imgO.src = "./assets/O.png";
+  imgX.src = "./assets/X.png";
   box.append(imgO);
-  
+
 })
