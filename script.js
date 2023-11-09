d8c437c80f3eeda27ea01ec893aab33fca1067d4
Isaac Rakstad <iscrakdev@gmail.com>
2023-11-09 11:42:18 -0600

    added reset button and refined functionality of addListeners

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function addListeners() {
-  let lastMove = true;
-  const boxes = document.getElementsByClassName("box");
+const boxes = document.getElementsByClassName("box");
 
+function addListeners() {
+  let currentMove = true;
+  
   for (let box of boxes)
     box.addEventListener("click", (e) => {
       const imgO = document.createElement("img");
@@ -10,30 +11,56 @@ function addListeners() {
       imgO.src = "./assets/O.png";
       imgX.src = "./assets/X.png";
 
-      if (isAvailable(box) && gameStatus() === "active") {
-        if (lastMove) {
-          box.append(imgO);
-        } else {
+      if (isAvailable(box)) {
+        if (currentMove) {
           box.append(imgX);
+        } else {
+          box.append(imgO);
         }
-        lastMove = !lastMove;
+        currentMove = !currentMove;
+
+        let gameState = gameStatus(box, boxParentId);
+        if (gameState === "winner") {
+          console.log("winner");
+        } else if (gameState === "draw") {
+          console.log("draw");
+        } else {
+          console.log("active");
+        }
       }
     });
 }
 
 function isAvailable(box) {
-  // checks if parent node is main??
-  return box.parentNode.id === "main" ? true : false;
+  return box.classList.contains('playable');
 }
 
-function gameStatus() {
+function gameStatus(box, boxParentId) {
+  let gameState = "active";
+  const boxArray = [
+    [boxes[0], boxes[1], boxes[2]],
+    [boxes[3], boxes[4], boxes[5]],
+    [boxes[6], boxes[7], boxes[8]],
+  ];
   // for each space
+  /* for the given box */
   // check horizontal, vertical and diagonal neighbors
+
   // if 3 consecutive matching squares, game is won
   // else if all squares occupied, game is draw
+  /* a count variable to get to 9 squares could also return game is draw if no winner is found*/
+
   // else game is still active
+
   // return winner, draw, or active
-  return "active";
+  return gameState;
 }
 
+function resetGame () {
+  for (let box of boxes) {
+    box.classList.add('playable')
+  }
+}
+
+resetGame()
 addListeners();
