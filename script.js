3fc71cb6da5e56672c2723f90460df77c74926a6
Isaac Rakstad <iscrakdev@gmail.com>
2023-11-09 10:45:36 -0600

    formatting

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 function addListeners() {
-  let lastMove = null;
+  let lastMove = true;
   const boxes = document.getElementsByClassName("box");
-  
-  for(let box of boxes) box.addEventListener('click', e => {
-    const imgO = document.createElement('img');
-    const imgX = document.createElement('img');
-    const box = e.target;
-    imgO.src = "./assets/O.png";
-    imgX.src = "./assets/X.png";
-    
-    if(isAvailable(box) && gameStatus() === 'active') {
-      if(lastMove === 'x') {
-        box.append(imgO);
-        lastMove = 'o';
-      } else {
-        box.append(imgX);
-        lastMove = 'x';
+
+  for (let box of boxes)
+    box.addEventListener("click", (e) => {
+      const imgO = document.createElement("img");
+      const imgX = document.createElement("img");
+      const box = e.target;
+      imgO.src = "./assets/O.png";
+      imgX.src = "./assets/X.png";
+
+      if (isAvailable(box) && gameStatus() === "active") {
+        if (lastMove) {
+          box.append(imgO);
+        } else {
+          box.append(imgX);
+        }
+        lastMove = !lastMove;
       }
-    } 
-  })
+    });
 }
 
 function isAvailable(box) {
+  // checks if parent node is main??
   return box.parentNode.id === "main" ? true : false;
 }
 
@@ -32,8 +33,7 @@ function gameStatus() {
   // else if all squares occupied, game is draw
   // else game is still active
   // return winner, draw, or active
-  return 'active'
+  return "active";
 }
-
 
 addListeners();
